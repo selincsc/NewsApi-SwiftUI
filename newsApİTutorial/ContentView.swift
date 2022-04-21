9738072dd3d09b55f77d5e2dfca7fa9d18aa41eb
Selin Çağlar <58820720+selincsc@users.noreply.github.com>
2022-04-21 21:59:32 +0300

    Initial Commit

---
+++ newsApİTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  newsApİTutorial
+//
+//  Created by Selin Çağlar on 21.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ newsApİTutorial/newsApI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsApI_TutorialApp.swift
+//  newsApİTutorial
+//
+//  Created by Selin Çağlar on 21.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct newsApI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
